be2101c6ef958cb77dad528244038601a71a3a19
mateus <mateussilvamn18@gmail.com>
2026-07-06 21:54:33 -0300

    automatizando o cadastro de produtos

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(3) #Pausa para carregar o site
+time.sleep(10) #Pausa para carregar o site
 pyautogui.click(x=613, y=556)
 pyautogui.write("xdd")
 pyautogui.press("tab")
@@ -22,3 +22,44 @@ time.sleep(3) #Pausa para carregar o site
 
 #3 Abrir a base de dados
 tabela = pd.read_csv("produtos.csv")
+
+#4 Cadastrar os produtos da tabela
+for linha in tabela.index:
+    #codigo
+    codigo = str(tabela.loc[linha,"codigo"])
+    pyautogui.click(x=721, y=389)
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    #marca
+    marca = tabela.loc[linha,"marca"]
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+
+    #tipo
+    tipo = tabela.loc[linha,"tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+
+    #categoria
+    categoria = str(tabela.loc[linha,"categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+
+    #preco_unitario
+    preco_unitario = str(tabela.loc[linha,"preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+
+    #custo
+    custo = str(tabela.loc[linha,"custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+
+    #obs
+    obs = str(tabela.loc[linha,"obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
